30a4e145413c7d5488d22188269ae56ba67d2096
Braden Carlson <bradenjcarlson@live.com>
2025-05-26 20:18:20 -0700

    Main File
    added main file for gui, which currently is simply a hello world program
    with a quit button.

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World").grid(row=0,column=0)
+ttk.Button(frm, text="Quit",command=root.destroy).grid(row=0,column=1)
+root.mainloop()
+
